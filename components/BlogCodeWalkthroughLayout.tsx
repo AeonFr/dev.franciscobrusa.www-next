417b02933b2e274344e3165e35b3d57fcb30da83
AeonFr <frankno.94@gmail.com>
2021-04-26 21:22:11 +0200

    upload new favicons

---
+++ components/BlogCodeWalkthroughLayout.tsx
@@ -0,0 +1,11 @@
+import Layout from "./Layout"
+
+export default BlogCodeWalkthrough() {
+  
+
+
+  return (
+    <Layout>
+    </Layout>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "../styles/CodeBlock.css";
 
+import Head from "next/head";
 import ColorSchemePicker from "../components/layout/ColorSchemePicker";
 import { useColorScheme } from "../hooks/useColorScheme";
 
@@ -8,6 +9,27 @@ function MyApp({ Component, pageProps }) {
   const [scheme, setScheme] = useColorScheme();
   return (
     <>
+      <Head key="favicon">
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+      </Head>
+
       <ColorSchemePicker
         scheme={scheme}
         handleSchemeChange={(ev) => setScheme(ev.target.value)}
