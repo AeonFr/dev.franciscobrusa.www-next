6e067c27f606dfe2449e584d2bd4bb152766ed07
francisco <francisco@filmin.es>
2021-05-28 21:05:43 +0200

    major refactor to the BlogCodeWalkthroughLayout component

--- components/BlogCodeWalkthroughLayout.tsx
+++ components/BlogCodeWalkthroughLayout.tsx
@@ -7,81 +7,50 @@ import {
 import { InView } from "react-intersection-observer";
 import Layout from "./Layout";
 import Container from "./Container";
-import dynamic from "next/dynamic";
 import blogPostStyles from "../styles/BlogPostLayout.module.css";
 import { components } from "./BlogPostLayout";
+import { useMediaQuery } from "react-responsive";
 
 export default function BlogCodeWalkthrough({ children, metadata }) {
   const steps = useMemo(
     () => divideIntoSteps(children).map(parseStep),
     [children]
   );
-  const [isInPresentationMode, setPresentationMode] = useState(false);
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const rootRef = useRef(null);
+  const { isInPresentationMode, fullscreenElementRef, currentSlide } =
+    usePresentationMode(steps);
+
+  // todo add resize handlers I guess
+  const isMobile = useMediaQuery({
+    query: "(max-width: 1200px), (max-height: 500px)",
+  });
+
+  // a simple "onMounted" hook to always use a SSR version first, but
+  // then immediately re-render to adapt to user's browser
+  // this is good practice for SEO and for progressive enhancement
+  const [isSSRVersion, setIsSSRVersion] = useState(true);
 
   useEffect(() => {
-    const handleKeyUp = (event: KeyboardEvent) => {
-      if (event.ctrlKey && event.key == "p") {
-        event.preventDefault();
+    if (typeof window === "undefined") {
+      return;
+    }
 
-        if (!isInPresentationMode) {
-          rootRef.current?.requestFullscreen();
-
-          // Some global fixes to make presentation mode look better. (TODO: make it revertable)
-          const $metaViewport = document.querySelector("meta[name='viewport']");
-          $metaViewport.setAttribute("content", "width=600");
-          if (window.innerWidth > 1000) {
-            document.documentElement.style.fontSize = "18px";
-          }
-        } else if (document.fullscreenElement) {
-          document.exitFullscreen();
-          window.location.reload();
-        }
-        setPresentationMode(!isInPresentationMode);
-      }
-
-      if (event.key == "ArrowRight" || event.key == "Right") {
-        if (currentSlide + 1 == steps.length) {
-          setPresentationMode(false);
-          setCurrentSlide(0);
-
-          if (document.fullscreenElement) {
-            document.exitFullscreen();
-            window.location.reload();
-          }
-        } else {
-          setCurrentSlide(currentSlide + 1);
-        }
-      }
-
-      if (
-        (event.key == "ArrowLeft" || event.key == "Left") &&
-        currentSlide - 1 !== -1
-      ) {
-        setCurrentSlide(currentSlide - 1);
-      }
-    };
-
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => window.removeEventListener("keyup", handleKeyUp);
-  }, [isInPresentationMode, currentSlide, steps]);
+    setIsSSRVersion(false);
+  }, []);
 
   return (
     <Layout {...metadata}>
-      <div ref={rootRef} style={{ backgroundColor: "var(--bg)" }}>
+      <div ref={fullscreenElementRef} style={{ backgroundColor: "var(--bg)" }}>
         <MDXProvider components={components}>
-          {!isInPresentationMode ? (
-            steps.map((step, i) => {
-              return <Step currentStep={i} steps={steps} key={i} />;
-            })
+          {isInPresentationMode ? ( // Only one step at a time in presentation mode
+            <PresentationModeStep currentStep={currentSlide} steps={steps} />
           ) : (
-            <Step
-              currentStep={currentSlide}
-              steps={steps}
-              isInPresentationMode={true}
-            />
+            steps.map((_, i) =>
+              isMobile || isSSRVersion ? ( // All basic steps for mobile
+                <BasicStep currentStep={i} steps={steps} key={i} />
+              ) : (
+                <DesktopStep currentStep={i} steps={steps} key={i} />
+              )
+            )
           )}
         </MDXProvider>
       </div>
@@ -121,201 +90,207 @@ interface StepProps {
   isInPresentationMode?: boolean;
 }
 
-const Step = dynamic(
-  Promise.resolve(({ currentStep, steps, isInPresentationMode }: StepProps) => {
-    const { originalChildren, restOfChildren, codeBlock, image } =
-      steps[currentStep];
+const BasicStep: React.FC<StepProps> = ({ currentStep, steps }) => {
+  const { originalChildren } = steps[currentStep];
 
-    const miniEditorProps: StatefulEditorProps = useMemo(
-      () => (codeBlock ? getEditorProps(codeBlock) : null),
-      [codeBlock]
-    );
+  return (
+    <Container style={{ maxWidth: "100%" }}>
+      <div className={blogPostStyles["Post-article"]}>
+        {originalChildren.map((child, i) => {
+          if (child.props.mdxType === "pre") {
+            return <MiniEditorWithState {...getEditorProps(child)} key={i} />;
+          } else return <div key={i}>{child}</div>;
+        })}
+      </div>
+    </Container>
+  );
+};
 
-    // for the desktop version
-    const [inViewStep, setInViewStep] = useState(0);
-    const [inViewSubstep, setInViewSubstep] = useState(0);
-    const isFirstStepWithCode = useMemo(
-      () => steps[currentStep - 1] && !steps[currentStep - 1]?.codeBlock,
-      [steps, currentStep]
-    );
-    const isOnlyStepWithCode = useMemo(
-      () =>
-        isFirstStepWithCode &&
-        steps[currentStep + 1] &&
-        !steps[currentStep + 1]?.codeBlock,
-      [isFirstStepWithCode, steps, currentStep]
-    );
-    const subSteps = useMemo(() => {
-      let result = [];
+const PresentationModeStep: React.FC<StepProps> = ({ currentStep, steps }) => {
+  const { restOfChildren, codeBlock, image } = steps[currentStep];
+  const miniEditorProps: StatefulEditorProps = useMemo(
+    () => (codeBlock ? getEditorProps(codeBlock) : null),
+    [codeBlock]
+  );
 
-      let i = currentStep;
-      while (steps[i] && steps[i].codeBlock) {
-        result.push(steps[i]);
-        i++;
-      }
-      return result;
-    }, [steps, currentStep]);
-    const subStepsEditorProps = useMemo(() => {
-      return subSteps.map(({ codeBlock }) => getEditorProps(codeBlock));
-    }, [subSteps]);
-
-    // todo add resize handlers I guess
-    const [isMobile] = useState(
-      window.innerWidth < 1200 || window.innerHeight < 500
-    );
-
-    // mobile version, no codeblock version & presentation mode: show every step one at a time
-    if (isInPresentationMode || isOnlyStepWithCode || !codeBlock || isMobile) {
-      return (
-        <Container style={{ maxWidth: "100%" }}>
-          <div
-            className={blogPostStyles["Post-article"]}
-            style={{
-              display: "grid",
-              gridTemplateColumns:
-                !isMobile && (codeBlock || image) ? "1fr 1fr" : "100%",
-              gap: "2rem",
-              padding: "2rem 0",
-              alignItems: "center",
-              ...(isInPresentationMode
-                ? {
-                    position: "fixed",
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    padding: "3rem",
-                    backgroundColor: "var(--bg)",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }
-                : {}),
-            }}
-          >
-            {!isInPresentationMode ? (
-              <div>
-                {originalChildren.map((child, i) => {
-                  if (child.props.mdxType === "pre") {
-                    return <MiniEditorWithState {...miniEditorProps} key={i} />;
-                  } else return <div key={i}>{child}</div>;
-                })}
-              </div>
-            ) : codeBlock ? (
-              <>
-                <div>{restOfChildren}</div>
-                <div
-                  style={{
-                    position: "relative",
-                  }}
-                >
-                  <MiniEditorWithState {...miniEditorProps} />
-                </div>
-              </>
-            ) : (
-              <>
-                <div>{restOfChildren}</div>
-                {image}
-              </>
-            )}
-          </div>
-        </Container>
-      );
-    }
-
-    // desktop version: show every step together with a sticky editor on the left,
-    // until theres a step without code
-
-    if (!isFirstStepWithCode) return null;
-
-    return (
-      <InView
-        as="div"
-        onChange={(inView) => inView && setInViewStep(currentStep)}
+  return (
+    <Container style={{ maxWidth: "100%" }}>
+      <div
+        className={blogPostStyles["Post-article"]}
+        style={{
+          display: "grid",
+          gridTemplateColumns: codeBlock || image ? "1fr 1fr" : "100%",
+          gap: "2rem",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          padding: "3rem",
+          backgroundColor: "var(--bg)",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-        <Container
-          className={blogPostStyles["Post-article"]}
-          style={{
-            width: "100%",
-            maxWidth: "80rem",
-            margin: "2rem auto",
-            display: "grid",
-            gap: "2rem",
-            gridTemplateColumns: "1fr 1fr",
-          }}
-        >
-          {subSteps.map(({ codeBlock, restOfChildren }, i) => {
-            return (
-              <div
-                key={`${currentStep}-${i}`}
-                className={blogPostStyles["Post-article"]}
-                style={{
-                  position: "relative",
-                  minHeight:
-                    subSteps[i + 1] !== undefined
-                      ? "100vh"
-                      : subStepsEditorProps[i].style.height,
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <InView
-                  as="div"
-                  onChange={(inView) => inView && setInViewSubstep(i)}
-                  threshold={1}
-                  style={{
-                    position: "absolute",
-                    top: "1rem",
-                    left: "-1rem",
-                    gridColumnStart: 1,
-                    height: "50%",
-                    maxHeight: "80vh",
-                    width: "2px",
-                  }}
-                  children={<span />}
-                />
-                <div>{restOfChildren}</div>
-              </div>
-            );
-          })}
-
-          <div
-            style={{
-              position: "relative",
-              gridColumnStart: 2,
-              gridRowStart: 1,
-              gridRowEnd: subSteps.length + 1,
-            }}
-          >
+        {codeBlock ? (
+          <>
+            <div>{restOfChildren}</div>
             <div
               style={{
-                position: "sticky",
-                top: "1rem",
-                height: "calc(100vh - 3rem)",
-                overflowY: "auto",
+                position: "relative",
+              }}
+            >
+              <MiniEditorWithState {...miniEditorProps} />
+            </div>
+          </>
+        ) : (
+          <>
+            <div>{restOfChildren}</div>
+            {image}
+          </>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+const DesktopStep: React.FC<StepProps> = ({ currentStep, steps }) => {
+  const { originalChildren, restOfChildren, codeBlock, image } =
+    steps[currentStep];
+
+  const miniEditorProps: StatefulEditorProps = useMemo(
+    () => (codeBlock ? getEditorProps(codeBlock) : null),
+    [codeBlock]
+  );
+
+  // for the desktop version
+  const [inViewStep, setInViewStep] = useState(0);
+  const [inViewSubstep, setInViewSubstep] = useState(0);
+  const isFirstStepWithCode = useMemo(
+    () => steps[currentStep - 1] && !steps[currentStep - 1]?.codeBlock,
+    [steps, currentStep]
+  );
+  const isOnlyStepWithCode = useMemo(
+    () =>
+      isFirstStepWithCode &&
+      steps[currentStep + 1] &&
+      !steps[currentStep + 1]?.codeBlock,
+    [isFirstStepWithCode, steps, currentStep]
+  );
+  const subSteps = useMemo(() => {
+    let result = [];
+
+    let i = currentStep;
+    while (steps[i] && steps[i].codeBlock) {
+      result.push(steps[i]);
+      i++;
+    }
+    return result;
+  }, [steps, currentStep]);
+  const subStepsEditorProps = useMemo(() => {
+    return subSteps.map(({ codeBlock }) => getEditorProps(codeBlock));
+  }, [subSteps]);
+
+  // The desktop layout can just be a simple layout for steps
+  // that have no codeblock or that are the only ones with code
+  // of their kind
+  if (!codeBlock || isOnlyStepWithCode) {
+    return <BasicStep currentStep={currentStep} steps={steps} />;
+  }
+
+  // desktop version aggregates all steps with code and paints them
+  // at once, which makes possible the sticky mini editor.
+  if (!isFirstStepWithCode) {
+    return null;
+  }
+
+  return (
+    <InView
+      as="div"
+      onChange={(inView) => inView && setInViewStep(currentStep)}
+    >
+      <Container
+        className={blogPostStyles["Post-article"]}
+        style={{
+          width: "100%",
+          maxWidth: "80rem",
+          margin: "2rem auto",
+          display: "grid",
+          gap: "2rem",
+          gridTemplateColumns: "1fr 1fr",
+        }}
+      >
+        {subSteps.map(({ codeBlock, restOfChildren }, i) => {
+          return (
+            <div
+              key={`${currentStep}-${i}`}
+              className={blogPostStyles["Post-article"]}
+              style={{
+                position: "relative",
+                minHeight:
+                  subSteps[i + 1] !== undefined
+                    ? "100vh"
+                    : subStepsEditorProps[i].style.height,
                 display: "flex",
                 alignItems: "center",
               }}
             >
-              <MiniEditorWithState
-                {...(inViewStep === currentStep
-                  ? subStepsEditorProps[inViewSubstep]
-                  : miniEditorProps)}
+              <InView
+                as="div"
+                onChange={(inView) => inView && setInViewSubstep(i)}
+                threshold={1}
                 style={{
-                  ...(inViewStep === currentStep
-                    ? subStepsEditorProps[inViewSubstep]
-                    : miniEditorProps
-                  ).style,
-                  width: "100%",
+                  position: "absolute",
+                  top: "1rem",
+                  left: "-1rem",
+                  gridColumnStart: 1,
+                  height: "50%",
+                  maxHeight: "80vh",
+                  width: "2px",
                 }}
+                children={<span />}
               />
+              <div>{restOfChildren}</div>
             </div>
+          );
+        })}
+
+        <div
+          style={{
+            position: "relative",
+            gridColumnStart: 2,
+            gridRowStart: 1,
+            gridRowEnd: subSteps.length + 1,
+          }}
+        >
+          <div
+            style={{
+              position: "sticky",
+              top: "1rem",
+              height: "calc(100vh - 3rem)",
+              overflowY: "auto",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <MiniEditorWithState
+              {...(inViewStep === currentStep
+                ? subStepsEditorProps[inViewSubstep]
+                : miniEditorProps)}
+              style={{
+                ...(inViewStep === currentStep
+                  ? subStepsEditorProps[inViewSubstep]
+                  : miniEditorProps
+                ).style,
+                width: "100%",
+              }}
+            />
           </div>
-        </Container>
-      </InView>
-    );
-  }),
-  { ssr: false }
-);
+        </div>
+      </Container>
+    </InView>
+  );
+};
 
 interface StepWithExtractedCodeBlock {
   originalChildren: React.ReactElement[];
@@ -371,3 +346,84 @@ function getEditorProps(codeBlock: React.ReactElement): StatefulEditorProps {
     },
   };
 }
+
+const usePresentationMode = (steps: StepWithExtractedCodeBlock[]) => {
+  const [isInPresentationMode, setPresentationMode] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const fullscreenElementRef = useRef(null);
+
+  const enter = () => {
+    setPresentationMode(true);
+    fullscreenElementRef.current?.requestFullscreen();
+  };
+
+  const leave = () => {
+    setPresentationMode(false);
+    setCurrentSlide(0);
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  };
+
+  const nextSlide = () => {
+    if (currentSlide + 1 === steps.length) {
+      leave();
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide > 0) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  const handleKeyEvent = (event: KeyboardEvent) => {
+    if (event.ctrlKey && (event.keyCode == 80 || event.key === "p")) {
+      event.preventDefault();
+
+      enter();
+
+      return false;
+    }
+
+    if (event.keyCode === 27 || event.key === "Esc" || event.key === "Escape") {
+      leave();
+    }
+
+    if (
+      event.keyCode === 39 ||
+      event.key === "ArrowRight" ||
+      event.key === "Right"
+    ) {
+      nextSlide();
+    }
+
+    if (
+      event.keyCode === 37 ||
+      event.key === "ArrowLeft" ||
+      event.key === "Left"
+    ) {
+      prevSlide();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyEvent);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyEvent);
+    };
+  }, [currentSlide, steps]);
+
+  return {
+    isInPresentationMode,
+    fullscreenElementRef,
+    enter,
+    leave,
+    currentSlide,
+  };
+};
