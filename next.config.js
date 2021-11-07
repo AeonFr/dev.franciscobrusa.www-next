fafc72c49152980bce15b091b43413cef8c1de97
AeonFr <frankno.94@gmail.com>
2021-11-07 20:26:07 +0100

    deprecate usage of custom vanilla-extract nextjs plugin, use the official plugin

--- next.config.js
+++ next.config.js
@@ -1,15 +1,51 @@
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
 });
-const withVanillaExtract = require("vanilla-extract-plugin-nextjs");
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+const {
+  getGlobalCssLoader,
+} = require("next/dist/build/webpack/config/blocks/css/loaders");
+const MiniCssExtractPlugin = require("next/dist/compiled/mini-css-extract-plugin");
 
 const baseURL = "https://franciscobrusa.dev";
 
-module.exports = withMDX(
-  withVanillaExtract({
-    pageExtensions: ["js", "jsx", "mdx"],
-    env: {
-      baseURL,
-    },
-  })
-);
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx"],
+  env: {
+    baseURL,
+  },
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    const cssRules = config.module.rules.find(
+      (rule) =>
+        Array.isArray(rule.oneOf) &&
+        rule.oneOf.some(
+          ({ test }) =>
+            typeof test === "object" &&
+            typeof test.test === "function" &&
+            test.test("filename.css")
+        )
+    ).oneOf;
+
+    cssRules.unshift({
+      test: /(?<!\.vanilla)\.css$/,
+      sideEffects: true,
+      use: getGlobalCssLoader(
+        {
+          assetPrefix: options.config.assetPrefix,
+          isClient: !isServer,
+          isServer,
+          isDevelopment: dev,
+        },
+        [],
+        []
+      ),
+    });
+
+    return config;
+  },
+};
+
+module.exports = withMDX(withVanillaExtract(nextConfig));
